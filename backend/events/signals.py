d2eedfc8de5436348b65ffd29cf635f8ff6016ba
chrinsley <jchrinsley02@gmail.com>
2026-09-04 18:07:22 +0400

    Add pydantic dependency

--- backend/events/signals.py
+++ backend/events/signals.py
@@ -6,4 +6,4 @@ from django.db.models.signals import post_save
 @receiver(post_save, sender=User, dispatch_uid="send_welcome_email")
 def send_welcome_email(sender,instance, created,**kwargs):
     if created:
-        send_mail("welcome", "Welcome to LocalEvent", "jchrinsley2@gmail.com", [instance.email], fail_silently=False)
+        send_mail("welcome", "Welcome to LocalEvent", "jchrinsley2@gmail.com", [instance.email], fail_silently=True)
