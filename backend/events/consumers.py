3e0f8b232b7280e5e78dabcbdb5b72da7d0bd564
chrinsley <jchrinsley02@gmail.com>
2026-09-10 09:58:24 +0400

    fix chatconsumer

--- backend/events/consumers.py
+++ backend/events/consumers.py
@@ -6,17 +6,23 @@ import json
 class ChatConsumer(AsyncWebsocketConsumer):
 
     async def connect(self):
-
+        user = self.scope.get("user")
         self.room_name = self.scope["url_route"]["kwargs"]["room_id"]
 
-        self.room_group_name = f"chat_{self.room_name}"
 
-        await self.channel_layer.group_add(
-            self.room_group_name,
-            self.channel_name
-        )
+        
+        if user.is_authenticated:
+            self.room_group_name = f"chat_{self.room_name}"
 
-        await self.accept()
+            await self.channel_layer.group_add(
+                self.room_group_name,
+                self.channel_name
+            )
+
+        else:
+            self.close()
+
+            
 
 
     async def receive(self, text_data):
