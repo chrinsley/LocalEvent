a3a139e16f9dff53b8d2e1eca9a96e8b6ad78a61
chrinsley <jchrinsley02@gmail.com>
2026-09-10 18:22:36 +0400

    fix chatconsumer

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -144,9 +144,10 @@ AWS_DEFAULT_ACL = None
 
 EMAIL_BACKEND = os.environ.get(
     'EMAIL_BACKEND',
-    'django.core.mail.backends.smtp.EmailBackend',
+    'django.core.mail.backends.console.EmailBackend' if DEBUG
+    else 'django.core.mail.backends.smtp.EmailBackend',
 )
-EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.gmail.com')
+EMAIL_HOST = os.environ.get('EMAIL_HOST', '')
 EMAIL_PORT = int(os.environ.get('EMAIL_PORT', '587'))
 EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'true').lower() == 'true'
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
--- backend/events/signals.py
+++ backend/events/signals.py
@@ -1,17 +1,9 @@
-from django.db.models.signals import post_save
-from django.conf import settings
 from django.core.mail import send_mail
 from django.dispatch import receiver
-
 from .models import User
+from django.db.models.signals import post_save
 
 @receiver(post_save, sender=User, dispatch_uid="send_welcome_email")
-def send_welcome_email(sender, instance, created, **kwargs):
+def send_welcome_email(sender,instance, created,**kwargs):
     if created:
-        send_mail(
-            "Welcome to LocalEvent",
-            "Welcome to LocalEvent",
-            settings.DEFAULT_FROM_EMAIL,
-            [instance.email],
-            fail_silently=False,
-        )
+        send_mail("welcome", "Welcome to LocalEvent", "jchrinsley2@gmail.com", [instance.email], fail_silently=True)
